15e5975949f442d468967a7b97502bb3f982e5de
Jun Zeng <junzeng.cool@163.com>
2022-03-26 17:18:13 +0800

    test for generating changed-id with commit-msg
    Change-Id: I0f054dda3929eb17629ed7ee1b2adb6525aff1fc
    Signed-off-by: Jun Zeng <junzeng.cool@163.com>

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main (int argc, char *argv[])
 {
+     int i;
      printf("hello world\n");
+     for (i = 0; i < 10; ++i)
+     {
+	  printf("%d", i);
+     }
+     printf("\n");
      return 0;
 }
